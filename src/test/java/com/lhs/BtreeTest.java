b89fb876af9ed6132356acd252a3cab7b99b6e0c
longhuashen <longhuashen@tobox.cn>
2021-02-26 18:38:10 +0800

    调整单元测试

--- src/test/java/com/lhs/BtreeTest.java
+++ src/test/java/com/lhs/BtreeTest.java
@@ -15,9 +15,14 @@ public class BtreeTest {
     @Test
     public void testPut() {
         BTree bTree = new BTree();
-        bTree.put("a".getBytes(), "b".getBytes());
+        bTree.put("a".getBytes(), "a".getBytes());
+        bTree.put("b".getBytes(), "b".getBytes());
+        bTree.put("c".getBytes(), "c".getBytes());
+        bTree.put("d".getBytes(), "d".getBytes());
+        bTree.put("e".getBytes(), "e".getBytes());
+        bTree.put("ac".getBytes(), "e".getBytes());
 
         byte[] bytes = bTree.get("a".getBytes());
-        Assert.assertArrayEquals("b".getBytes(), bytes);
+        Assert.assertArrayEquals("a".getBytes(), bytes);
     }
 }
